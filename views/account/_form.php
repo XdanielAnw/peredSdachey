b6743b2d40db37fc73460e52950d26372780953b
XdanielAnw <dbaiguzhinov11@mail.ru>
2026-04-03 14:42:34 +0300

    4.3 орректное заполнение формы

--- views/account/_form.php
+++ views/account/_form.php
@@ -1,5 +1,7 @@
 <?php
 
+use app\models\Category;
+use app\models\Level;
 use yii\bootstrap5\Html;
 use yii\bootstrap5\ActiveForm;
 
@@ -18,17 +20,25 @@ use yii\bootstrap5\ActiveForm;
 
     <?= $form->field($model, 'description')->textarea(['rows' => 6]) ?>
 
-    <?= $form->field($model, 'category_id')->textInput() ?>
+    <?= $form->field($model, 'category_id')->dropDownList(
+        Category::getCategory(), ['prompt' => 'Выберите категорию']
+    ) ?>
 
-    <?= $form->field($model, 'level_id')->textInput() ?>
+    <?= $form->field($model, 'level_id')->dropDownList(
+        Level::getLevel(), ['prompt' => 'Выберите сложность']
+    ) ?>
 
-    <?= $form->field($model, 'cook_time_id')->textInput() ?>
+    <?= $form->field($model, 'cook_time_id')->dropDownList(
+        Level::getLevel(), ['prompt' => 'Выберите время готовки']
+    ) ?>
 
-    <?= $form->field($model, 'date_end')->textInput() ?>
+    <?= $form->field($model, 'date_end')->textInput(['type' => 'date']) ?>
 
-    <?= $form->field($model, 'contact')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'contact')->widget(\yii\widgets\MaskedInput::class, [
+            'mask' => '+7(999)-999-99-99',
+    ]) ?>
 
-    <?= $form->field($model, 'photo')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'photo')->textInput(['type' => 'file']) ?>
 
     <div class="form-group">
         <?= Html::submitButton('Опубликовать', ['class' => 'btn btn-success']) ?>
